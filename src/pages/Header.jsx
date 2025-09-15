e1cb1b2ded2cc84ad796685671a20f1c3d03ba33
ravipatelctf <ravipatelctf@gmail.com>
2025-09-15 22:41:54 +0530

    chore:add github link of repository in header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,8 @@
+import { FaGithub } from "react-icons/fa6"; 
 
 export default function Header() {
     return (
-        <div className="container py-3 bg-black">
+        <div className="container py-3 bg-black d-flex align-items-center justify-content-between">
             <div className="d-flex gap-2 align-items-center">
                 <span className="text-success fw-bold fs-4">{`[`}</span>
                 <span className="text-white fs-4 fw-bold">"Ravi", </span>
@@ -9,6 +10,7 @@ export default function Header() {
                 <span className="text-white fs-4 fw-bold">"Patel"</span>
                 <span className="text-success fw-bold fs-4">{`]`}</span>
             </div>
+            <a href="https://github.com/ravipatelctf/developer-portfolio"><FaGithub size={25} color="white" /></a>
         </div>
     )
 }
